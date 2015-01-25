b146880698ebbf9cb61d621c84a2022219fa2e3e
DPr00f <jocolopes@gmail.com>
2015-01-25 18:12:09 +0000

    Updated to work with wercker

--- tests/UnitTestCase.php
+++ tests/UnitTestCase.php
@@ -5,7 +5,11 @@ abstract class UnitTestCase extends PHPUnit_Framework_TestCase{
 	protected $steamApiKey;
 
 	public function __construct(){
-		$this->steamApiKey = require __DIR__ . "/../test-steam-api-key.php";
+		$key = getenv('STEAM_API_KEY');
+		if(!$key){
+			$key = require __DIR__ . "/../test-steam-api-key.php";
+		}
+		$this->steamApiKey = $key;
 	}
 
 }
